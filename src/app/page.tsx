aa0b60cf100c87aa1838f0a30917a36f68243dd3
JoshVC <josuevalenzuela87@gmail.com>
2025-07-07 12:58:44 -0400

    feat: Only Slider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,11 @@
-import MainSection from "@/components/MainSection";
 import Slider from "../components/slider/Slider";
-import FeaturedContent from "@/components/FeaturedContent";
-import NewsSection from "@/components/NewsSection";
+
 
 export default function Home() {
   return (
     <>
       <Slider/>
-      <MainSection/>
-      <FeaturedContent/>
-      <NewsSection/>
+      
     </>
   );
 }
